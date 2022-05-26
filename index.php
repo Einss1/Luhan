e2e30dd5f024a7cee6df2bb8ab574adf10dc1736
Einss1 <luan_navarro@hotmail.com>
2022-05-26 20:47:22 +0100

    Commit 17

--- index.php
+++ index.php
@@ -4,45 +4,114 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="A fun and easy way to learn!">
-    <title>Luhan</title>
-    <style><?php include 'CSS/index.css'; ?></style>
-</head>
-
-<body>
-    <header class="header">
-        <img class="header-image" src="Images/monkeylogo.jpeg" alt="Luhan logo">
-        <nav class="header-menu">
-            <a class="header-menu-item" href="login.php">Login</a>
-            <a class="header-menu-item" href="signup.php">Register</a>
-        </nav>
-    </header>
-
-    <main class="content"> 
-        <section class="content-main">
-            <div class="content-main-info">
-                <h1 class="content-main-info-title">Luhan</h1>
-                <h2 class="content-main-info-subtitle">A fun and easy way to learn Japanese!</h2>
-                <button class="content-main-info-button" onclick="window.location.href='signup.php'">Join Us!</button>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Lumorah</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="Images/monkeylogo.png" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body class="d-flex flex-column h-100">
+        <main class="flex-shrink-0">
+            <!-- Navigation-->
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container px-5">
+                    <a class="navbar-brand" href="index.php">Lumorah</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+                            <li class="nav-item"><a class="nav-link" href="signup.php">Register</a></li>
+                            <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- Header-->
+            <header class="bg-dark py-5">
+                <div class="container px-5">
+                    <div class="row gx-5 align-items-center justify-content-center">
+                        <div class="col-lg-8 col-xl-7 col-xxl-6">
+                            <div class="my-5 text-center text-xl-start">
+                                <h1 class="display-5 fw-bolder text-white mb-2">Lumorah</h1>
+                                <p class="lead fw-normal text-white-50 mb-4">A fun and easy way to learn Japanese!</p>
+                                <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                                    <a class="btn btn-primary btn-lg px-4 me-sm-3" href="register.php">Get Started</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img class="img-fluid rounded-3 my-5" src="Images/logo-2.png" alt="..." /></div>
+                    </div>
+                </div>
+            </header>
+            <!-- Features section-->
+            <section class="py-5" id="features">
+                <div class="container px-5 my-5">
+                    <div class="row gx-5">
+                        <div class="col-lg-4 mb-5 mb-lg-0"><h2 class="fw-bolder mb-0">Our objectives</h2></div>
+                        <div class="col-lg-8">
+                            <div class="row gx-5 row-cols-1 row-cols-md-2">
+                                <div class="col mb-5 h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-cash-coin"></i></div>
+                                    <h2 class="h5">Free</h2>
+                                    <p class="mb-0">We understand that learning sometimes can be expensive. Our mission is to make it free and accessible!</p>
+                                </div>
+                                <div class="col mb-5 h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-peace"></i></div>
+                                    <h2 class="h5">Easy to use</h2>
+                                    <p class="mb-0">Learning something new is already hard enough. Why make it even harder?</p>
+                                </div>
+                                <div class="col mb-5 mb-md-0 h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-emoji-wink"></i></div>
+                                    <h2 class="h5">Fun</h2>
+                                    <p class="mb-0">Our main priority is to get you to have fun while learning!</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Testimonial section-->
+            <div class="py-5 bg-light">
+                <div class="container px-5 my-5">
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-lg-10 col-xl-7">
+                            <div class="text-center">
+                                <div class="fs-4 mb-4 fst-italic">"Lumorah started as an idea between a college student and his wife. For months the idea was left and forgotten, until we decided to dive back into it. Becoming our most fulfilling project!"</div>
+                                <div class="d-flex align-items-center justify-content-center">
+                                    <img class="rounded-circle me-3" src="Images/luan.jpg" alt="Luan, co-owner" />
+                                    <div class="fw-bold">
+                                        Luan Silva
+                                        <span class="fw-bold text-primary mx-1">/</span>
+                                        Co-owner, Lumorah
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <img class="content-main-image" src="Images/logo-2.png" alt="Logo">
-        </section>
-
-        <section class="content-secondary">
-            <h3 class="content-secondary-title">Our objectives</h3>
-            <p class="content-secondary-paragraph">Free!</p>
-            <p class="content-secondary-paragraph">Easy to use!</p>
-            <p class="content-secondary-paragraph">Fun!</p>
-        </section>
-    </main>
-
-    <footer class="footer">
-        <img class="footer-image" src="Images/logo2.png" alt="Luhan">
-    </footer>
-</body>
-
+        </main>
+        <!-- Footer-->
+        <footer class="bg-dark py-4 mt-auto">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; Lumorah 2022</div></div>
+                    <div class="col-auto">
+                        <a class="link-light small" href="#!">Terms</a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light small" href="#!">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
 </html>
